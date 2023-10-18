b7dbe3b13a9a57ba7afe1a6be79bcdb9feb83479
lavesh4807 <142747980+lavesh4807@users.noreply.github.com>
2023-10-18 14:35:38 +0530

    pwm.py

--- pwm.py
+++ pwm.py
@@ -1,35 +1,28 @@
 import RPi.GPIO as GPIO 
 import time
 
-# Set the GPIO mode to use Broadcom SOC channel numbering
 GPIO.setmode(GPIO.BCM)
 
-# Define GPIO pins for the Trigger, Echo, and Buzzer
 TRIGGER_PIN = 15
 ECHO_PIN = 13
 BUZZER_PIN =4
 
-# Set up the GPIO pins as inputs or outputs
 GPIO.setup(TRIGGER_PIN, GPIO.OUT)
 GPIO.setup(ECHO_PIN, GPIO.IN)
 GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
-# Initialize the Buzzer PWM (Pulse Width Modulation) controller
 buzzer_pwm = GPIO.PWM(BUZZER_PIN, 1000)  # Frequency = 1000 Hz
 buzzer_pwm.start(0)  # Start with a 0% duty cycle
 
-# Define a function to measure distance using the ultrasonic sensor
 def measure_distance():
     # Trigger the HC-SR04 ultrasonic sensor
     GPIO.output(TRIGGER_PIN, GPIO.HIGH)
     time.sleep(0.00001)
     GPIO.output(TRIGGER_PIN, GPIO.LOW)
 
-    # Record the start time when the ultrasonic pulse is sent
     while GPIO.input(ECHO_PIN) == False:
         start_time = time.time()
 
-    # Record the finish time when the echo is received
     while GPIO.input(ECHO_PIN) == True:
         finish_time = time.time()
 
@@ -57,7 +50,6 @@ try:
         # Adjust the PWM duty cycle for the buzzer based on the distance ratio
         buzzer_pwm.ChangeDutyCycle(distance_ratio)
         
-        # Delay for a short time before taking the next distance reading
         time.sleep(0.1)
 
 except KeyboardInterrupt:
